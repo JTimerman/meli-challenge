d94fd6f1e46c5191a867380dab9319cdb740f282
JTimerman <joaquin.timerman@gmail.com>
2020-01-25 15:57:00 -0300

    Add logo for the app

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,7 +7,11 @@ import "./Header.scss";
 export function Header() {
   return (
     <header>
-      Logo
+      <img
+        alt="Mercado Libre"
+        className="header-logo"
+        src="https://http2.mlstatic.com/ui/navigation/5.3.7/mercadolibre/logo__small.png"
+      />
       <SearchBox />
     </header>
   );
