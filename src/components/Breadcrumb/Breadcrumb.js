a0750baf98b12aa11a95914fe8b18bc3c94b41d5
JTimerman <joaquin.timerman@gmail.com>
2020-01-26 19:15:28 -0300

    Improve Breadcrumb styles

--- src/components/Breadcrumb/Breadcrumb.js
+++ src/components/Breadcrumb/Breadcrumb.js
@@ -3,13 +3,13 @@ import "./Breadcrumb.scss";
 
 export function Breadcrumb({ categories }) {
   return (
-    <ul className={"breadcrumb"}>
+    <ol className={"breadcrumb"}>
       {categories &&
         categories.map(category => (
           <li className={"breadcrumb-item"} key={category}>
             {category}
           </li>
         ))}
-    </ul>
+    </ol>
   );
 }
