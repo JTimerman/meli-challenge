04b29a625705b3ccc3cfec9844dbd04b33fe329b
JTimerman <joaquin.timerman@gmail.com>
2020-01-26 19:29:25 -0300

    Add functionality to the SearchBox

--- src/components/SearchBox/SearchBox.js
+++ src/components/SearchBox/SearchBox.js
@@ -1,21 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
 import "./SearchBox.scss";
 
 export function SearchBox() {
+  const [query, setQuery] = useState("");
+  let history = useHistory();
+
+  function searchQuery(event) {
+    event.preventDefault();
+    history.push(`/items?search=${query}`);
+  }
+
   return (
-    <form className="search-box-form" role="search">
+    <form className="search-box-form" role="search" onSubmit={searchQuery}>
       <input
         type="text"
         className="search-box-input"
         aria-label="Ingresá lo que quieras encontrar"
         placeholder="Nunca dejes de buscar"
         maxLength="120"
-        autoFocus=""
-        autoCapitalize="off"
-        autoCorrect="off"
-        spellCheck="false"
-        autoComplete="off"
         tabIndex="2"
+        onChange={event => setQuery(event.target.value)}
       />
       <button type="submit" className="search-box-btn" tabIndex="3" />
     </form>
